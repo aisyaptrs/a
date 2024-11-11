fcd1d38a212ec906943123b63ca1beda930ffc69
aisyaptrs <icaaa2973@gmail.com>
2024-11-11 23:05:12 +0700

    hapus semua kecuali title lagi

--- GelarRasa/Airbnb-Dashboard/pages/First_Page.py
+++ GelarRasa/Airbnb-Dashboard/pages/First_Page.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-from folium.plugins import MarkerCluster
-from streamlit_folium import st_folium
 
 # Judul aplikasi Streamlit
 st.title("New York Airbnb Dashboard")
